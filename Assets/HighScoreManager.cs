676a12e7082367049ec8e1cca9383c49226b6355
DESKTOP-7TVRN8T\lewis <lewispp@hotmail.com>
2018-03-24 20:03:04 +0100

    Added delete,insert and show to database

--- Assets/HighScoreManager.cs
+++ Assets/HighScoreManager.cs
@@ -11,7 +11,10 @@ public class HighScoreManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         connectionString  = "URI=file:" + Application.dataPath + "/HighScoreDB.sqlite";
+        InsertScore("Duncan", 0);
+        DeleteScore(2);
         GetScores();
+
 
 	}
 	
@@ -19,6 +22,29 @@ public class HighScoreManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+    private void InsertScore(string name, int newScore)
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("INSERT INTO HighScores(Name,LevelsCompleted) VALUES(\"{0}\",\"{1}\")",name,newScore);
+
+                dbCmd.CommandText = sqlQuery;
+                dbCmd.ExecuteScalar();
+                dbConnection.Close();
+
+               
+            }
+
+        }
+
+    }
+
 
     private void GetScores()
     {
@@ -48,5 +74,25 @@ public class HighScoreManager : MonoBehaviour {
         }
 
         
+    }
+
+    private void DeleteScore(int id)
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("DELETE FROM HighScores WHERE PlayerID = \"{0}\"", id);
+
+                dbCmd.CommandText = sqlQuery;
+                dbCmd.ExecuteScalar();
+                dbConnection.Close();
+
+
+            }
+
+        }
     }
 }
